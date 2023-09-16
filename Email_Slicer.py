e8f4f4248f4171cc05281ae707740113e73f7c97
Abdo Abo Samra <67236632+AbdoAbosamra@users.noreply.github.com>
2023-09-16 18:58:15 +0300

    Code V1

---
+++ Email_Slicer.py
@@ -0,0 +1,6 @@
+email = input("Please Enter your Email")
+components = email.split("@")
+username = components[0]
+domain = components[1]
+print(f"The username for this user is  : {username}")
+print(f"The domain for this email is : {domain}")
